135efa5af38acd926ccd339a41a5e55afb334b10
jeanriffel <jeanriffel1@gmail.com>
2022-01-16 08:29:13 -0300

    test: adding test order

--- test/integration/Order.test.ts
+++ test/integration/Order.test.ts
@@ -1,9 +1,16 @@
 import { Item } from "../../src/entities/Item";
 import { Order } from "../../src/entities/Order";
 
+let item_1 : Item;
+let item_2 : Item;
+
+beforeAll( ()=>{
+    item_1 = new Item(1, 'smart watch ', 58.68);
+    item_2 = new Item(2, 'smart tv ', 300.50);
+} );
+
+
 test('Creating order', ()=>{
-    const item_1 = new Item(1, 'smart watch ', 58.68);
-    const item_2 = new Item(2, 'smart tv ', 300.50);
     const order = new Order();
     order.addItem(item_1);
     order.addItem(item_2);    
