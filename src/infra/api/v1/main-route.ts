b4b12b40b4cf10a984ac545ecd171a615a16c7b5
jeanriffel <jeanriffel1@gmail.com>
2022-01-17 21:54:35 -0300

    feat: adding redis

--- src/infra/api/v1/main-route.ts
+++ src/infra/api/v1/main-route.ts
@@ -1,22 +1,27 @@
 import Router from 'koa-router';
 import { OrderItems } from '../../../application/dto/OrderItems';
 import { OrderController } from './order-controller';
+import Redis from 'ioredis';
+import JSONCache from 'redis-json';
 
 const mainRoute = new Router();
+const redis = new Redis();
+const jsonCache = new JSONCache(redis);
 
-mainRoute.post('/order', (ctx)=>{
-
-    const id : number = ctx.request.body.id;
+mainRoute.post('/order', async (ctx)=>{
+    
     const description : string = ctx.request.body.description;
     const price : number = ctx.request.body.price;
+    const orderId = Math.round(Math.random() * 100);
 
-    const orderItems = new OrderItems( id, description, price  );
+    //const result = await jsonCache.get(orderId );
+    await jsonCache.set(orderId.toString() ,  ctx.request.body);
+    
+    const orderItems = new OrderItems( orderId, description, price  );
 
     const orderController = new OrderController(orderItems);
-    ctx.response.body = "executed";
-
+    ctx.response.body = "Order number is ".concat( orderId.toString());
 } );
-
 
 export default  mainRoute;
 
