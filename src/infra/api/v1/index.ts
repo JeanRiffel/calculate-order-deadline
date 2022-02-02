f1ac908ecba2bf6836215a124da5eddcc091de11
jeanriffel <jeanriffel1@gmail.com>
2022-02-01 22:20:34 -0300

    WIP

--- src/infra/api/v1/index.ts
+++ src/infra/api/v1/index.ts
@@ -1,5 +1,7 @@
 import server from './server';
 const port = process.env.PORT;
 
+
+///running the server
 server.listen( port ,  
         ()=>{console.log(`Server is running on port ${port} ` )}) ;
